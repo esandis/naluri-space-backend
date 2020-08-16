7fc9243439be466753a0c30db7c5f0f9e01b3e92
Esandis Rio <rio@numeropro.com.au>
2020-08-16 13:34:39 +0800

    Added extra plugins for REST.

--- src/Controller/PiController.php
+++ src/Controller/PiController.php
@@ -2,11 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Pi Controller.
+ * @Route("/api", name="api_")
+ */
 class PiController
 {
-    public function getLatestPiValue(): Response
+    /**
+     * Return latest Pi Value.
+     * @Rest\Get("/pi")
+     *
+     * @return Response
+     */
+    public function getLatestPiValue()
     {
         // $pi = $this->countPiValue();
         $pi = 3.14;
